21bba17f76355db61f67f319ed984d2b0d49ffe9
Andreas Fink <afink@dam-united.com>
2025-07-28 18:31:25 +0200

    Switch Gitea action to use GITEA_TOKEN and log release payload data

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ URL=${SERVER_URL}/api/v1/repos/${REPOSITORY}/releases
 DATA="{ \"body\":\"${BODY}\", \"draft\": ${DRAFT}, \"name\": \"Just a release name\", \"tag_name\": \"${TAG}\", \"prerelease\": false }"
 echo "Using $URL"
 echo "Using tag ${TAG} for release"
+echo "Data: ${DATA}"
 git tag $TAG
 git push origin $TAG
 curl --request POST \
