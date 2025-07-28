62b5b6bb80fb5e571bde613b82e01ba7e950d663
Andreas Fink <afink@dam-united.com>
2025-07-28 18:13:53 +0200

    Add support for draft releases in Gitea action

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 URL=${SERVER_URL}/api/v1/repos/${REPOSITORY}/releases
+DATA="{ \"body\":\"${BODY}\", \"draft\": ${DRAFT}, \"name\": \"Just a release name\", \"tag_name\": \"${TAG}\", \"prerelease\": false }"
 echo "Using $URL"
 echo "Using tag ${TAG} for release"
 git tag $TAG
@@ -7,4 +8,4 @@ curl --request POST \
           --url "$URL" \
           --header 'content-type: application/json' \
           --header "Authorization: token ${TOKEN}" \
-          --data "{ \"body\":\"${BODY}\", \"draft\": false, \"name\": \"Just a release name\", \"tag_name\": \"${TAG}\", \"prerelease\": false }"
+          --data "$DATA"
