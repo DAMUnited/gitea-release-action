9765c8d91bc9a16611a33a5c4e98b09fcd54f396
Andreas Fink <afink@dam-united.com>
2025-07-28 18:37:56 +0200

    Log TOKEN value in release script for debugging purposes

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 URL=${SERVER_URL}/api/v1/repos/${REPOSITORY}/releases
-DATA="{ \"body\":\"${BODY}\", \"draft\": ${DRAFT}, \"name\": \"Just a release name\", \"tag_name\": \"${TAG}\", \"prerelease\": false }"
+DATA="{ \"body\":\"${BODY}\", \"draft\": ${DRAFT}, \"name\": \"${NAME}\", \"tag_name\": \"${TAG}\", \"prerelease\": false }"
 echo "Using $URL"
 echo "Using tag ${TAG} for release"
 echo "Data: ${DATA}"
+echo "TOKEN: ${TOKEN}"
 git tag $TAG
 git push origin $TAG
 curl --request POST \
